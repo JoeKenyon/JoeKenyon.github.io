60e2093d67d6ff046055b93e6eb81f53530425aa
JoeBoiii72 <joekenyon72@gmail.com>
2020-06-06 21:33:42 +0100

    Finished projects page

--- script.js
+++ script.js
@@ -37,7 +37,53 @@ function INDEX_onLoad()
         skills.appendChild(skillbox);
         
     }
+}
 
-    
+function getJSONDATA(yourUrl){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
 
+function PROJECTS_onLoad()
+{
+    // div to add to
+    var skills = document.getElementById("PROJECTS");
+
+    var api_url = "https://api.github.com/users/JoeBoiii72/repos?"
+
+    var json_obj = JSON.parse(getJSONDATA(api_url));
+
+    for(i in json_obj)
+    {
+        var name = json_obj[i].name;
+        var lang = json_obj[i].language;
+        var desc = json_obj[i].description;
+
+        if(desc.length <= 1)
+        {
+            desc = "N/A";
+        }
+
+        var projectBox = document.createElement("div");
+        projectBox.className = "project";
+
+        var nameBox = document.createElement("div");
+        nameBox.id = "pName";
+        nameBox.innerHTML = name;
+        projectBox.appendChild(nameBox);
+
+        var langBox = document.createElement("div");
+        langBox.id = "pLang";
+        langBox.innerHTML = lang;
+        projectBox.appendChild(langBox);
+
+        var descBox = document.createElement("div");
+        descBox.id = "pDesc";
+        descBox.innerHTML = desc;
+        projectBox.appendChild(descBox);
+        
+        skills.appendChild(projectBox); 
+    }
 }
