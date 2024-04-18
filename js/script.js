ba92099b3f7ac816e6436c63e5539826eee59c24
Joe Kenyon <joekenyon72@gmail.com>
2024-04-18 12:46:42 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,32 +3,38 @@
 // need to update these as we add them i guess
 var art_pics = 
 [
-	"/Users/joekenyon/Desktop/website/images/dreams.png",
-	"/Users/joekenyon/Desktop/website/images/animal.png",
-	"/Users/joekenyon/Desktop/website/images/alone.png",
-	"/Users/joekenyon/Desktop/website/images/shitfuck.png",
-	"/Users/joekenyon/Desktop/website/images/Untitled.png",
-	"/Users/joekenyon/Desktop/website/images/misterblisterman.png",
-	"/Users/joekenyon/Desktop/website/images/Talk_to_us_4.png",
-	"/Users/joekenyon/Desktop/website/images/IMG_20230313_013040_680.jpg",
-	"/Users/joekenyon/Desktop/website/images/20231117_122750.jpg",
-	"/Users/joekenyon/Desktop/website/images/20231117_123046.jpg",
-	"/Users/joekenyon/Desktop/website/images/FB_IMG_1696683447435.jpg",
-	"/Users/joekenyon/Desktop/website/images/20240417_195438.jpg",
-	"/Users/joekenyon/Desktop/website/images/20231208_21cxvcx3655-1.png",
-	"/Users/joekenyon/Desktop/website/images/20240417_195450.jpg",
-	"/Users/joekenyon/Desktop/website/images/bitmaCHARp.png",
-	"/Users/joekenyon/Desktop/website/images/bitmasdasdsadap.png",
-	"/Users/joekenyon/Desktop/website/images/20231117_123005-1.png",
-	"/Users/joekenyon/Desktop/website/images/dsasdsad.jpg",
-	"/Users/joekenyon/Desktop/website/images/ingoude. (2).png",
-	"/Users/joekenyon/Desktop/website/images/Joe Kenyon (1).png",
-	"/Users/joekenyon/Desktop/website/images/Talk to us (1).png",
-	"/Users/joekenyon/Desktop/website/images/20231208_213655-1.png",
-	"/Users/joekenyon/Desktop/website/images/20240417_195356.jpg",
-	"/Users/joekenyon/Desktop/website/images/unknown.png",
-	"/Users/joekenyon/Desktop/website/images/Your_paragraph_text_2.png"
+	"/images/dreams.png",
+	"/images/animal.png",
+	"/images/alone.png",
+	"/images/shitfuck.png",
+	"/images/Untitled.png",
+	"/images/misterblisterman.png",
+	"/images/Talk_to_us_4.png",
+	"/images/IMG_20230313_013040_680.jpg",
+	"/images/20231117_122750.jpg",
+	"/images/20231117_123046.jpg",
+	"/images/FB_IMG_1696683447435.jpg",
+	"/images/20240417_195438.jpg",
+	"/images/20231208_21cxvcx3655-1.png",
+	"/images/20240417_195450.jpg",
+	"/images/bitmaCHARp.png",
+	"/images/bitmasdasdsadap.png",
+	"/images/20231117_123005-1.png",
+	"/images/dsasdsad.jpg",
+	"/images/ingoude. (2).png",
+	"/images/Joe Kenyon (1).png",
+	"/images/Talk to us (1).png",
+	"/images/20231208_213655-1.png",
+	"/images/20240417_195356.jpg",
+	"/images/unknown.png",
+	"/images/Your_paragraph_text_2.png"
 ];
+
+
+var path = document.location.pathname;
+var working_dir = path.substring(path.indexOf('/'), path.lastIndexOf('/'));
+
+
 
 function img_clk(img_ref)
 {
@@ -72,11 +78,11 @@ function img_clk(img_ref)
 }
 
 function set_up()
-{	
+{		
 	for (var i = 0; i < art_pics.length; ++i)
 	{
 		var img = document.createElement('img'); // Make a new image.
-		img.src = art_pics[i];
+		img.src = working_dir + art_pics[i];
 		img.className += "art";
 		img.onclick = function () { img_clk(this);}
 		
